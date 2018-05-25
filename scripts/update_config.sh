9e0d17d4a384610ddd3ba75ca951995c54aa1ab1
starun96 <tarunsaharya@gmail.com>
2018-05-25 08:38:35 -0400

    changed i3 config

--- scripts/update_config.sh
+++ scripts/update_config.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-path_to_config_i3=~/.config/i3/config
-path_to_config_dunst=~/.config/dunst/dunstrc
-path_to_config_compton=~/.compton.conf
-path_to_config_vscode_settings=~/.config/Code/User/settings.json
-path_to_config_vscode_keybindings=~/.config/Code/User/keybindings.json
+local=~/.config
+remote=~/customDE/configs
 
-cp configs/i3 $path_to_config_i3
-cp configs/compton $path_to_config_compton
-cp configs/dunst $path_to_config_dunst
-cp configs/vscode_settings.json $path_to_config_vscode_settings
-cp configs/vscode_keybindings.json $path_to_config_vscode_keybindings
+cp $remote/i3 $local/i3/config
+cp $remote/compton $local/i3/compton.conf
+cp $remote/dunst $local/dunst/dunstrc
+cp $local/Code/User/settings.json $remote/vscode_settings.json
+cp $local/Code/User/keybindings.json $remote/vscode_keybindings.json
+cp -r $local/autokey $remote/autokey
